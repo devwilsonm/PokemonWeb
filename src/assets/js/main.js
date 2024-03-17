3a746fe4a3ab9b338908f6f4243868ee666d54a4
devwilsonm <devwilsonm@gmail.com>
2024-03-16 19:45:40 -0500

    ADD Slider +  Card pokemon

---
+++ src/assets/js/main.js
@@ -0,0 +1,31 @@
+var slideIndex = 0;
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    showSlides();
+  });
+
+function showNavBar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
